6c211d39ee9bbc91e941a188c896be5fedfada36
AnushreeKumar <112722298+silentswag@users.noreply.github.com>
2024-09-27 23:01:44 +0530

    basic upload

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI,UploadFile, File, Form, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi import Request
+from pdf2image import convert_from_path
+import pytesseract
+from PIL import Image
+import tempfile
+import os
+
+
 
 app = FastAPI()
 
@@ -20,3 +27,33 @@ async def read_form(request: Request):
 async def register(username: str = Form(...), password: str = Form(...)):
     # Here you would typically save the user information to a database
     return {"message": f"User {username} registered successfully!"}
+
+@app.post("/upload")
+async def upload_files(file: UploadFile=File(...)):
+    if not file.filename.endswith((".jpg",".jpeg")):
+        raise HTTPException(status_code=400, detail="Only JPG and JPEG files are allowed.")
+    
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg" if file.filename.endswith('.jpg') else ".jpeg") as temp_file:
+        temp_file.write(await file.read())
+        temp_file_path = temp_file.name
+
+    try:
+    # Open the image file
+        image = Image.open(temp_file_path)
+         # Perform OCR on the image
+        ocr_extracted = pytesseract.image_to_string(image)
+
+        return {"filename": file.filename, "text": ocr_extracted.strip()}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred during OCR processing: {str(e)}")
+    
+    
+        # Perform OCR on the image
+"""     ocr_extracted = pytesseract.image_to_string(image)
+            extracted+= pytesseract.image_to_string(image)+"\n"
+        return {"filename": file.filename, "text": ocr_extracted.strip()}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred during OCR processing: {str(e)}")"""
+
+    
